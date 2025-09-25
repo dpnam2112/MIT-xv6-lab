3908b9b914a99e78f54db09fe4e967e8e3ab53ce
dpnam2112 <dpnam2112@gmail.com>
2025-09-25 23:09:50 +0700

    superpage allocation api

--- kernel/kalloc.c
+++ kernel/kalloc.c
@@ -10,6 +10,7 @@
 #include "defs.h"
 
 void freerange(void *pa_start, void *pa_end);
+void freerange_superpg(void *pa_start, void *pa_end);
 
 extern char end[]; // first address after kernel.
                    // defined by kernel.ld.
@@ -21,13 +22,15 @@ struct run {
 struct {
   struct spinlock lock;
   struct run *freelist;
+  struct run *superpg_freelist;
 } kmem;
 
 void
 kinit()
 {
   initlock(&kmem.lock, "kmem");
-  freerange(end, (void*)PHYSTOP);
+  freerange_superpg(end, (void*)SUPERPG_STOP);
+  freerange((void*) SUPERPG_STOP, (void*)PHYSTOP);
 }
 
 void
@@ -37,6 +40,14 @@ freerange(void *pa_start, void *pa_end)
   p = (char*)PGROUNDUP((uint64)pa_start);
   for(; p + PGSIZE <= (char*)pa_end; p += PGSIZE)
     kfree(p);
+}
+
+void
+freerange_superpg(void *pa_start, void *pa_end) {
+  char *p;
+  p = (char*)SUPERPGROUNDUP((uint64)pa_start);
+  for(; p + SUPERPGSIZE <= (char*)pa_end; p += SUPERPGSIZE)
+    superfree(p);
 }
 
 // Free the page of physical memory pointed at by pa,
@@ -78,5 +89,42 @@ kalloc(void)
 
   if(r)
     memset((char*)r, 5, PGSIZE); // fill with junk
+  return (void*)r;
+}
+
+// superalloc and superfree: similar to their counterparts (kalloc and kfree),
+// but are used for superpages.
+void
+superfree(void *pa)
+{
+  struct run *r;
+
+  if(((uint64)pa % SUPERPGSIZE) != 0 || (char*)pa < end || (uint64)pa >= SUPERPG_STOP)
+    panic("superfree");
+
+  // Fill with junk to catch dangling refs.
+  memset(pa, 1, SUPERPGSIZE);
+
+  r = (struct run*)pa;
+
+  acquire(&kmem.lock);
+  r->next = kmem.superpg_freelist;
+  kmem.superpg_freelist = r;
+  release(&kmem.lock);
+}
+
+void *
+superalloc(void)
+{
+  struct run *r;
+
+  acquire(&kmem.lock);
+  r = kmem.superpg_freelist;
+  if(r)
+    kmem.superpg_freelist = r->next;
+  release(&kmem.lock);
+
+  if(r)
+    memset((char*)r, 5, SUPERPGSIZE); // fill with junk
   return (void*)r;
 }
